c49c72a8694d852728efe478931249ddd1bc560f
yayoimizuha <poko.peko.tomo@icloud.com>
2023-02-09 23:15:51 +0900

    update

--- main.cpp
+++ main.cpp
@@ -4,11 +4,18 @@
 #include "argh/argh.h"
 #include <fstream>
 #include <filesystem>
+#include "array_calculator.h"
 
 using namespace argh;
 using namespace std;
 using namespace bmp;
 using namespace filesystem;
+
+struct color {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+};
 
 int main(int argc, char **argv) {
 
@@ -27,6 +34,28 @@ int main(int argc, char **argv) {
         cerr << "File not exist!! " << image_name << endl;
         return EXIT_FAILURE;
     }
+    float rotate;
+    int move_horizontal, move_vertical;
+    float scale;
+    cmdl({"scale", "s"}, 1.0f) >> scale;
+    cmdl({"move_horizontal", "x"}, 0) >> move_horizontal;
+    cmdl({"move_vertical", "y"}, 0) >> move_vertical;
+    cmdl({"rotate", "r"}, 0.0f) >> rotate;
+
     Bitmap image;
     image.load(image_name);
+    auto height = image.height();
+    auto width = image.width();
+    vector<vector<color>> image_vec(height);
+    for (int h = 0; h < height; ++h) {
+        image_vec.at(h).resize(width);
+        for (int w = 0; w < width; ++w) {
+            auto pixel = image.get(w, h);
+            auto &i = image_vec.at(h).at(w);
+            i.r = pixel.r;
+            i.g = pixel.g;
+            i.b = pixel.b;
+        }
+    }
+    cout << unsigned(image_vec.at(100).at(100).r) << endl;
 }
